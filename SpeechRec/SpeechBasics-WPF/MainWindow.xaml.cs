1f93f2b576cfee929acac5f02df84b0d7aca94b3
Rui Li <kendallli@live.com>
2014-02-28 10:46:46 -0600

    Voice Rec
    Increased Accuracy

---
+++ SpeechRec/SpeechBasics-WPF/MainWindow.xaml.cs
@@ -0,0 +1,255 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.SpeechBasics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.IO;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Documents;
+    using System.Windows.Media;
+    using Microsoft.Kinect;
+    using Microsoft.Speech.AudioFormat;
+    using Microsoft.Speech.Recognition;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable",
+        Justification = "In a full-fledged application, the SpeechRecognitionEngine object should be properly disposed. For the sake of simplicity, we're omitting that code in this sample.")]
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Resource key for medium-gray-colored brush.
+        /// </summary>
+        private const string MediumGreyBrushKey = "MediumGreyBrush";
+
+
+
+        /// <summary>
+        /// Active Kinect sensor.
+        /// </summary>
+        private KinectSensor sensor;
+
+        /// <summary>
+        /// Speech recognition engine using audio data from Kinect.
+        /// </summary>
+        private SpeechRecognitionEngine speechEngine;
+
+
+        /// <summary>
+        /// List of all UI span elements used to select recognized text.
+        /// </summary>
+        private List<Span> recognitionSpans;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        /// <summary>
+        /// Gets the metadata for the speech recognizer (acoustic model) most suitable to
+        /// process audio from Kinect device.
+        /// </summary>
+        /// <returns>
+        /// RecognizerInfo if found, <code>null</code> otherwise.
+        /// </returns>
+        private static RecognizerInfo GetKinectRecognizer()
+        {
+            foreach (RecognizerInfo recognizer in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                string value;
+                recognizer.AdditionalInfo.TryGetValue("Kinect", out value);
+                if ("True".Equals(value, StringComparison.OrdinalIgnoreCase) && "en-US".Equals(recognizer.Culture.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return recognizer;
+                }
+            }
+            
+            return null;
+        }
+
+        /// <summary>
+        /// Execute initialization tasks.
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                try
+                {
+                    // Start the sensor!
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    // Some other application is streaming from the same Kinect sensor
+                    this.sensor = null;
+                }
+            }
+
+            if (null == this.sensor)
+            {
+                this.statusBarText.Text = Properties.Resources.NoKinectReady;
+                return;
+            }
+
+            RecognizerInfo ri = GetKinectRecognizer();
+
+            if (null != ri)
+            {
+
+                this.speechEngine = new SpeechRecognitionEngine(ri.Id);
+
+                
+                //Use this code to create grammar programmatically rather than from
+                //a grammar file.
+                
+                var directions = new Choices();
+                directions.Add(new SemanticResultValue("hello", "HELLO"));
+                directions.Add(new SemanticResultValue("hi", "HELLO"));
+                directions.Add(new SemanticResultValue("bye", "BYE"));
+                directions.Add(new SemanticResultValue("goodbye", "BYE"));
+                directions.Add(new SemanticResultValue("reset", "RESET"));
+                directions.Add(new SemanticResultValue("startover", "RESET"));
+                var gb = new GrammarBuilder { Culture = ri.Culture };
+                gb.Append(directions);
+                
+                var g = new Grammar(gb);
+
+                speechEngine.LoadGrammar(g);
+                /***
+                // Create a grammar from grammar definition XML file.
+                using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(Properties.Resources.SpeechGrammar)))
+                {
+                    var g = new Grammar(memoryStream);
+                    speechEngine.LoadGrammar(g);
+                }
+                ***/
+                speechEngine.SpeechRecognized += SpeechRecognized;
+                speechEngine.SpeechRecognitionRejected += SpeechRejected;
+
+                // For long recognition sessions (a few hours or more), it may be beneficial to turn off adaptation of the acoustic model. 
+                // This will prevent recognition accuracy from degrading over time.
+                ////speechEngine.UpdateRecognizerSetting("AdaptationOn", 0);
+
+                speechEngine.SetInputToAudioStream(
+                    sensor.AudioSource.Start(), new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else
+            {
+                this.statusBarText.Text = Properties.Resources.NoSpeechRecognizer;
+            }
+        }
+
+        /// <summary>
+        /// Execute uninitialization tasks.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.AudioSource.Stop();
+
+                this.sensor.Stop();
+                this.sensor = null;
+            }
+
+            if (null != this.speechEngine)
+            {
+                this.speechEngine.SpeechRecognized -= SpeechRecognized;
+                this.speechEngine.SpeechRecognitionRejected -= SpeechRejected;
+                this.speechEngine.RecognizeAsyncStop();
+            }
+        }
+        /**
+        /// <summary>
+        /// Remove any highlighting from recognition instructions.
+        /// </summary>
+        private void ClearRecognitionHighlights()
+        {
+            foreach (Span span in recognitionSpans)
+            {
+                if (span != null && recognitionSpans!= null)
+
+                    span.Foreground = (Brush)this.Resources[MediumGreyBrushKey];
+                    span.FontWeight = FontWeights.Normal;
+            }
+        }
+        **/
+        /// <summary>
+        /// Handler for recognized speech events.
+        /// </summary>              
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Speech utterance confidence below which we treat speech as if it hadn't been heard
+            const double ConfidenceThreshold = 0.7;
+
+            String youSaidText = "You said:";
+            String KinectSaysText = "Kinect says:";
+            String speechRecConfText = "Confidence:";
+            //ClearRecognitionHighlights();
+            this.SpeechKinectGot.Text = youSaidText + e.Result.Semantics.Value.ToString();
+            this.SpeechRecConf.Text = speechRecConfText + e.Result.Confidence.ToString();
+            if (e.Result.Confidence >= ConfidenceThreshold)
+            //if (e.Result.Semantics.Value.ToString() != null && !e.Result.Semantics.Value.ToString().Equals(""))
+            {
+                switch (e.Result.Semantics.Value.ToString())
+                {
+                    case "HELLO":
+                        this.FeedbackText.Text = KinectSaysText + "Hello there.";
+                        break;
+
+                    case "BYE":
+                        this.FeedbackText.Text = KinectSaysText + "Bye, see you soon.";
+                        break;
+                    case "RESET":
+                        this.FeedbackText.Text = "Reseted";
+                        break;
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for rejected speech events.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            //ClearRecognitionHighlights();
+        }
+    }
+}
