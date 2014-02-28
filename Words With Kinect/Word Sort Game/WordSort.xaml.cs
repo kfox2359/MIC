d97c51366aeda18bee390c7e3383ab14d74554e3
tayjkohn <tayjkohn@aim.com>
2014-02-28 12:51:53 -0600

    tk implementing wordsort game

---
+++ Words With Kinect/Word Sort Game/WordSort.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace Words_With_Kinect.Word_Sort_Game
+{
+    /// <summary>
+    /// Interaction logic for WordSort.xaml
+    /// </summary>
+
+    public partial class WordSort : UserControl
+    {
+        private KinectSensor kinect;
+        private MainWindow window;
+
+        public WordSort(MainWindow window, KinectSensor kinect)
+        {
+            this.kinect = kinect;
+            this.window = window;
+            InitializeComponent();
+            kinectRegion.KinectSensor = kinect;
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            window.Content = new WordSortGameScreen(window, kinect);
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            window.Content = new Games(window, kinect);
+        }
+    }
+}
---
+++ Words With Kinect/Word Sort Game/WordSortGameScreen.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace Words_With_Kinect.Word_Sort_Game
+{
+    /// <summary>
+    /// Interaction logic for WordSortGameScreen.xaml
+    /// </summary>
+    public partial class WordSortGameScreen : UserControl
+    {
+        private KinectSensor kinect;
+        private MainWindow window;
+        public WordSortGameScreen(MainWindow window, KinectSensor kinect)
+        {
+            this.kinect = kinect;
+            this.window = window;
+            InitializeComponent();
+            kinectRegion.KinectSensor = kinect;
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            window.Content = new WordSort(window, kinect);
+        }
+    }
+}
